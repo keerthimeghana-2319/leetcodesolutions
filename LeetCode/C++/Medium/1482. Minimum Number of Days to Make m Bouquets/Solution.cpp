1e052e0542e0fc2c279782912f38927068f1e47b
KEERTHI MEGHANA CHITIKILA <meghanachitikila@gmail.com>
2026-08-28 18:12:00 +0530

    Add Medium C++ Solution for 1482. Minimum Number of Days to Make m Bouquets from LeetCode

---
+++ LeetCode/C++/Medium/1482. Minimum Number of Days to Make m Bouquets/Solution.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+bool helper(vector<int>&bloonDay,int days, int m,int k)
+{
+    int flowers = 0;
+    int boquets = 0;
+    for(int i = 0 ; i < bloonDay.size() ;i++)
+    {
+        if(bloonDay[i] <= days)
+        {
+            flowers++;
+        }
+        else
+        {
+            boquets += flowers/k;
+            flowers = 0;
+        }
+    }
+    boquets += flowers/k;
+    return boquets >= m;
+}
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        if(bloomDay.size() < m*k) return -1;
+        int low = *min_element(bloomDay.begin(),bloomDay.end());
+        int high = *max_element(bloomDay.begin(),bloomDay.end());
+        while(low <= high)
+        {
+            int mid = low+(high-low)/2;
+            if(helper(bloomDay,mid,m,k))
+            {
+                high = mid-1;
+            }
+            else
+            {
+                low = mid+1;
+            }
+        }
+        return low;
+    }
+};
