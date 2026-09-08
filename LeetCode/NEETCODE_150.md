3d2a06600adae141ff309094fc3211fcf524ff8a
KEERTHI MEGHANA CHITIKILA <meghanachitikila@gmail.com>
2026-09-08 14:34:19 +0530

    Add Medium C++ Solution for 567. Permutation in String from LeetCode

--- LeetCode/NEETCODE_150.md
+++ LeetCode/NEETCODE_150.md
@@ -3,7 +3,7 @@
 Track your progress on the NeetCode 150 roadmap practice problems.
 
 ## Progress
-- **Completed:** 2 / 150 (1.3%)
+- **Completed:** 3 / 150 (2.0%)
 
 ---
 
@@ -31,7 +31,7 @@ Track your progress on the NeetCode 150 roadmap practice problems.
 - [ ] Best Time to Buy and Sell Stock
 - [ ] Longest Substring Without Repeating Characters
 - [ ] Longest Repeating Character Replacement
-- [ ] Permutation in String
+- [x] [Permutation in String](./C++/Medium/567. Permutation in String/)
 - [ ] Minimum Window Substring
 - [ ] Sliding Window Maximum
 
---
+++ LeetCode/Patterns/Sliding Window/567. Permutation in String/Solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n2 = s2.size();
+        int n1 = s1.size();
+        reverse(s1.begin(),s1.end());
+        int l = 0;
+        int r = l+n1;
+        string temp = "";
+        for(int i = l ; i < r ; i++)
+        {
+            temp += s2[i];
+        }
+        if(temp == s1)
+        {
+            return true;
+        }
+        for(int j = r ; j < n2 ; j++)
+        {
+            temp.erase(0, 1);
+            l++;
+            temp += s2[r];
+            r++;
+            if(temp == s1)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
