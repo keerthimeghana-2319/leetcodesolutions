35a242ae12f69ae58d1867279c6a9f8836662470
KEERTHI MEGHANA CHITIKILA <meghanachitikila@gmail.com>
2026-09-15 15:30:28 +0530

    Add Easy C++ Solution for 20. Valid Parentheses from LeetCode

--- LeetCode/Patterns/Stack & Queue/20. Valid Parentheses/README.md
+++ LeetCode/Patterns/Stack & Queue/20. Valid Parentheses/README.md
@@ -8,8 +8,8 @@
 String, Stack, Bracket Sequences
 
 ### 🚀 Performance
-- **Runtime:** 0 ms
-- **Memory:** 10.3 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Patterns/Stack & Queue/20. Valid Parentheses/Solution.cpp
+++ LeetCode/Patterns/Stack & Queue/20. Valid Parentheses/Solution.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<int>res;
+        stack<char>res;
         if(s == "[" || s == "]" || s == "(" || s == ")" || s == "{" || s == "}")
         {
             return false;
         }
-        for(char k: s)
+        for(char k : s)
         {
-            if(k == '(' || k == '[' || k == '{')
+            if(k == '['|| k == '(' || k == '{')
             {
                 res.push(k);
             }
             else
             {
                 if(res.empty()) return false;
-                char top = res.top();
+                int top = res.top();
                 res.pop();
-                if((k == ')' && top != '(') || (k == ']' && top != '[') || (k == '}' && top != '{'))
-                return false;
+                if(k == ']' && top != '[' || k == ')' && top != '(' || k == '}' && top != '{')
+                {
+                    return false;
+                }
             }
         }
         return res.empty();
