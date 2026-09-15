758df7040351be916b7879c47c0d8e5f4b9abf1d
KEERTHI MEGHANA CHITIKILA <meghanachitikila@gmail.com>
2026-09-15 15:13:42 +0530

    Add Easy C++ Solution for 20. Valid Parentheses from LeetCode

---
+++ LeetCode/Patterns/Stack & Queue/20. Valid Parentheses/Solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int>res;
+        if(s == "[" || s == "]" || s == "(" || s == ")" || s == "{" || s == "}")
+        {
+            return false;
+        }
+        for(char k: s)
+        {
+            if(k == '(' || k == '[' || k == '{')
+            {
+                res.push(k);
+            }
+            else
+            {
+                if(res.empty()) return false;
+                char top = res.top();
+                res.pop();
+                if((k == ')' && top != '(') || (k == ']' && top != '[') || (k == '}' && top != '{'))
+                return false;
+            }
+        }
+        return res.empty();
+    }
+};
